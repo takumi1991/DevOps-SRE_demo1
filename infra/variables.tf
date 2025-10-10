306776a340842aa5cf1f137717d6806af606d946
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-10 21:55:35 +0900

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,14 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "asia-northeast1" }
-variable "slack_auth_token"  { type = string  sensitive = true }
-variable "slack_webhook_url" { type = string  sensitive = true }
+variable "project_id" {
+  type = string
+  # 例: "devops-sre-demo1"  ← 既にTF CloudのTerraform Variablesで設定済にする
+}
+
+variable "region" {
+  type    = string
+  default = "asia-northeast1"  # 東京。別リージョンなら変更可
+}
+
+variable "google_credentials_json" {
+  type      = string
+  sensitive = true  # TF CloudのTerraform Variablesに「JSON全文」を貼付け済
+}
