0536ca19f79d35eff3709b3fbf88c6c8483b53b9
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-10 22:45:55 +0900

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,3 @@
-output "project_id"   { value = var.project_id }
-output "region"       { value = var.region }
-output "cluster_name" { value = google_container_cluster.autopilot.name }
-output "repo_url"     {
-  value = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}"
+output "cloud_run_url" {
+  value = google_cloud_run_v2_service.hello.uri
 }
